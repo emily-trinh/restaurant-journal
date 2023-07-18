6dfd93e7b5fb6ad8e9c525be05d28156b16f73f6
emily-trinh <etrinh26@gmail.com>
2023-07-18 14:54:10 -0700

    Created test classes for Journal and Restaurant

--- src/main/model/Restaurant.java
+++ src/main/model/Restaurant.java
@@ -6,18 +6,23 @@ import static jdk.nashorn.internal.objects.ArrayBufferView.length;
 // Represents a restaurant with given name, rating, and optional quick review.
 public class Restaurant {
 
-    private String name;
+    private final String name;
     private Integer rating;
     private String review;
     private static Integer MAX_REVIEW_LENGTH;
 
-    // constructs a restaurant to be added to journal
+    // REQUIRES: length of restaurant name > 0 and restaurant rating must be from [1, 10]
+    // EFFECTS: constructs a restaurant to be added to journal
     public Restaurant(String restaurantName, Integer restaurantRating) {
         name = restaurantName;
         rating = restaurantRating;
         MAX_REVIEW_LENGTH = 50;
     }
 
+
+    // REQUIRES: review length > 0
+    // MODIFIES: this
+    // EFFECTS: adds review to restaurant and returns a success or failure message
     public String addReview(String restaurantReview) {
         if (length(restaurantReview) <= MAX_REVIEW_LENGTH) {
             return "Review added!";
@@ -25,4 +30,21 @@ public class Restaurant {
             return "Review too long!";
         }
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+
+    public String getReview() {
+        return review;
+    }
+
 }
